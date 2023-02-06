e37a4f726b79a2d38de3e69d68d70224fc91767d
Matthew Soulanille <matthew@soulanille.net>
2023-02-06 15:35:09 -0800

    Fix cors

--- tensorflow_lite_support/web/tflite_model_runner/demo/start.sh
+++ tensorflow_lite_support/web/tflite_model_runner/demo/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cp -f src/* dist/
-npx http-server dist/
+npx node-static -a 0.0.0.0 -H '{"Cross-Origin-Opener-Policy": "same-origin", "Cross-Origin-Embedder-Policy": "require-corp"}' -p 8000 dist/
